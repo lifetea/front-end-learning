39f09c984bb3896e5de483fa1c6e6013742d4e16
lifetea <494886251@qq.com>
2016-11-07 21:11:56 +0800

    fixs #2 algorithm sort select

---
+++ algorithm/sort/Select.js
@@ -0,0 +1,23 @@
+/**
+ * Created by lifetea on 2016/11/7.
+ */
+const example = require('../../util/Example');
+
+example.build('选择排序先排小',function () {
+
+    let arr = [3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48]
+
+    for(let [i,len] = [0,arr.length];i < len;i++){
+        let min = arr[i];
+        let k   = i;
+        for(let j = 1+i;j < len; j++){
+            if(min > arr[j]){
+                k = j;
+                min = arr[j]
+            }
+        }
+        arr[k] = arr[i]
+        arr[i] = min
+    }
+    console.log(arr)
+})
