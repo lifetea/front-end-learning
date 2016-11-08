48c42b2318c28db258b9dccba876ce11b60c8247
lifetea <494886251@qq.com>
2016-11-08 11:55:23 +0800

    fixs#4 ES5 正则

---
+++ es5/Regular.js
@@ -0,0 +1,38 @@
+/**
+ * Created by lifetea on 2016/11/8.
+ */
+const example  =  require('../util/Example')
+
+example.build('定义正则',function () {
+    let reg = RegExp('e','i')
+    let str = 'hello world'
+    let val = str.match(reg)
+    console.log(val)
+})
+
+
+example.build('正则字面量',function () {
+
+    let reg = /e/i
+    let str = 'hello world'
+    let val = str.match(reg)
+    console.log(val)
+
+    function createReg() {
+        let re = /e/i
+        reg.foo = 'bar'
+        return re
+    }
+
+    let re1 = createReg()
+    let re2 = createReg()
+
+    console.log(re1 == re2) // es5 和 es6 中正则字面量都是生成一个新的正则
+
+    re2.foo = 'baz'
+
+    console.log('re1',re1)
+
+    console.log('re2',re2)
+
+})
