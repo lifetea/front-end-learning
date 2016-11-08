a3f55a69e9dd497a6d2589437858d71ea928f7d7
lifetea <494886251@qq.com>
2016-11-08 10:51:12 +0800

    fixs #2 前端算法 插入排序

---
+++ algorithm/sort/Insert.js
@@ -0,0 +1,21 @@
+/**
+ * Created by lifetea on 2016/11/8.
+ */
+const example = require('../../util/Example');
+
+example.build('插入排序数组',function () {
+
+    let arr = [3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48]
+
+    for(let [i,len] = [1,arr.length];i < len;i++){
+        for(let j = i; j > 0;j--){
+            if(arr[j] < arr[j-1]){
+               let temp = arr[j]
+                arr[j]  = arr[j-1]
+               arr[j-1] = temp
+            }
+        }
+    }
+
+    console.log(arr)
+})
