54f5afecf1b9531e72d918bdbedf72480368ff83
lifetea <494886251@qq.com>
2016-11-09 15:54:59 +0800

    fixs #6 函数 回调

---
+++ es5/Function/recursive.js
@@ -0,0 +1,22 @@
+/**
+ * Created by lifetea on 2016/11/9.
+ * email: 494886251@qq.com
+ * github: https://github.com/lifetea
+ */
+const example = require('../../util/Example')
+
+
+example.build('阶乘函数',function () {
+    let factorial = function (x) {
+        if(x == 0){
+            return 1
+        }else{
+            return x * factorial(x-1)
+        }
+    }
+
+    let res = factorial(5)
+
+    console.log(res)
+
+})
