f2c9efc3040e312c049869e74f2c56da7d88a7de
lifetea <494886251@qq.com>
2016-11-10 14:13:19 +0800

    fixs #5 正则 字符

--- es5/RegExp/character.js
+++ es5/RegExp/character.js
@@ -20,36 +20,55 @@ example.build('反斜杠backslash',function () {
 
 
 example.build('点匹配任意字符 . ',function () {
-    let reg = /.$/i
-    let a   = 'hello world'
-    let b   = ' hello world+1'
-    let ra  = a.match(reg)
-    let rb  = b.match(reg)
+    {
+        let reg = /.$/i
+        let str = 'hello world'
+        let res = reg.exec(str)
+        console.log(res)
+        //匹配单个字符 除了 line terminators: \n, \r, \u2028 or \u2029.
+    }
+    {
+        let reg = /[^]$/i
+        let str = 'hello world'
+        let res = reg.exec(str)
+        console.log(res)
+        //匹配单个字符 除了 line terminators: \n, \r, \u2028 or \u2029.
+    }
 
-    console.log(ra)
-
-    console.log(rb)
-    //匹配单个字符 除了 line terminators: \n, \r, \u2028 or \u2029.
 })
 
 
-example.build('任何ASCII \\w ',function () {
-    let reg = /\w/i
-    let str = 'hello world'
-    let res = reg.exec(str)
-
-    console.log(res)
+example.build('任何ASCII字符 \\w ',function () {
+    {
+        let reg = /\w/i
+        let str = 'hello world'
+        let res = reg.exec(str)
+        console.log(res)
+    }
+    {
+        let reg = /[a-zA-Z0-9_]/i
+        let str = 'hello world'
+        let res = reg.exec(str)
+        console.log(res)
+    }
 })
 
 
-example.build('任何非ASCII \\W ',function () {
-
-    let reg = /\W/i
-    let str = 'hello world'
-    let res = reg.exec(str)
-
-    console.log(res)
-    //匹配到了空格
+example.build('任何非ASCII字符 \\W ',function () {
+    {
+        let reg = /\W/i
+        let str = 'hello world'
+        let res = reg.exec(str)
+        console.log(res)
+        //匹配到了空格
+    }
+    {
+        let reg = /[^0-9a-zA-Z_]/i
+        let str = 'hello world'
+        let res = reg.exec(str)
+        console.log(res)
+        //匹配到了空格
+    }
 })
 
 
@@ -84,20 +103,34 @@ example.build('unicode非空白字符 \\S ',function () {
 
 
 example.build('任何ASCII数字 \\d ',function () {
-    let reg = /\d/i
-    let str = 'hello world 3'
-    let res = reg.exec(str)
-
-    console.log(res)
+    {
+        let reg = /\d/i
+        let str = 'hello world 3'
+        let res = reg.exec(str)
+        console.log(res)
+    }
+    {
+        let reg = /[0-9]/i
+        let str = 'hello world 3'
+        let res = reg.exec(str)
+        console.log(res)
+    }
 
 })
 
 
 example.build('非ASCII数字 \\D ',function () {
-    let reg = /\D/i
-    let str = 'hello world 3'
-    let res = reg.exec(str)
-
-    console.log(res)
+    {
+        let reg = /\D/i
+        let str = 'hello world 3'
+        let res = reg.exec(str)
+        console.log(res)
+    }
+    {
+        let reg = /[^0-9]/i
+        let str = 'hello world 3'
+        let res = reg.exec(str)
+        console.log(res)
+    }
 
 })
