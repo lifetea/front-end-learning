9b5e81116f44389a434a1e0afc5ee0e48b835446
lifetea <494886251@qq.com>
2016-11-13 14:38:50 +0800

    fixs #5 正则 分词 生成驼峰

--- trial/RegExp/camel-format.js
+++ trial/RegExp/camel-format.js
@@ -3,13 +3,25 @@
  */
 const example = require('../../util/Example')
 
-example.build('驼峰转换',function () {
+example.build('转成驼峰',function () {
     let str = 'do-change-status-test'
     let reg = /(\w)(\w*)-?/g
     let res = str.replace(reg,function (match,p1,p2,offset) {
         console.log(match,p1,p2,offset)
         let first  = offset == 0 ? p1 : p1.toUpperCase()
-        return [p1,p2].join('')
+        return [first,p2].join('')
+    })
+    console.log(res)
+})
+
+
+example.build('解析驼峰',function () {
+    let str = 'doChangeStatusTest'
+    let reg = /(?:(?:[a-z]|[A-Z])?[a-z]*)/g
+    let res = str.replace(reg,function (match,offset) {
+        console.log(match,offset)
+        let str = (offset != 0 && !!match) ? '-' : ''
+        return ['',match.toLowerCase()].join(str)
     })
     console.log(res)
 })
