0e162a380c47cc24253eb7cf3a9706a8794ac6c0
lifetea <494886251@qq.com>
2016-11-13 17:23:14 +0800

    fixs #5 正则 字符串 最后一个单词大小

---
+++ trial/RegExp/get-last-word-length.js
@@ -0,0 +1,14 @@
+/**
+ * Created by lifetea on 2016/11/13.
+ * email: 494886251@qq.com
+ * github: https://github.com/lifetea
+ */
+const example = require('../../util/Example')
+
+example.build('获取字符串最后单词长度(正则)',function () {
+    let str = 'hello world '
+    let reg = /(\s*)(\w*)\1$/i
+    let [,,world] = reg.exec(str)
+    let len = world.length
+    console.log(len)
+})
