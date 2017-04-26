cf56f388636cd3e16afd2ce82774eefbf8f81beb
lifetea <494886251@qq.com>
2017-04-26 22:17:29 +0800

    fixs #1

---
+++ lesson/js/01/01.js
@@ -0,0 +1,45 @@
+/**
+ * Created by Administrator on 2017/4/24/024.
+ */
+// 当这个01.js文件加载好之后立即执行 里面的内容
+(function () {
+    // dan hang
+    /*
+    var a = 1;
+    var b = 2;
+    */
+    // "hello world" //字面量
+    // 1 //字面量
+    // var a = 1;
+    // var A = 2;
+    // var str = "hello world";
+    // console.log(str)
+    // console.log("hello world")
+
+
+
+    //函数一
+    // function fun(){
+    //     return 1
+    // }
+    //函数二
+    // var fun = function () {
+    //      return 1
+    // }
+    //
+    // var val = fun()
+    //
+    // console.log("val:",val)
+
+    var x = 1;
+    var y = 2;
+    // var z =
+    // x++
+    //
+    // y
+    //
+    // console.log(z)
+    // console.log(x)
+
+})()
+// const example  =  require('../util/Example')
